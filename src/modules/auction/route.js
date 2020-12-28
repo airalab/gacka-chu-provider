dfa2764795d5a54a9a595108236fceba9ef1ae3a
vol4tim <vol4tim@gmail.com>
2020-12-28 13:06:42 +0400

    up

--- src/modules/auction/route.js
+++ src/modules/auction/route.js
@@ -3,7 +3,6 @@ import controller from "./controller";
 
 const router = express.Router();
 
-router.get("/", controller.home);
 router.get("/list", controller.list);
 router.get("/auction/:id", controller.auction);
 router.post("/demand", controller.demand);
